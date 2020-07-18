111b316a9e4b73e09ef1eadb136684005a0cdedc
Abhishek Khatri <abhishek.khatri@publicissapient.com>
2020-07-18 23:15:16 +0530

    get loggin in user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const auth = require("../../middleware/auth");
 const router = express.Router();
 
-// @route   GET api/profiles
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Profiles route"));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route   GET api/profiles/me
+// @desc    Get current users profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: "There is no profile for this user " });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
