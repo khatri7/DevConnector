62b16c4bc9780e5991e4b58d00a4af7fd15ea42a
Abhishek Khatri <abhishekkhatri98@gmail.com>
2020-08-04 23:51:19 +0530

    posts like unlike delete

---
+++ client/src/actions/post.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { setAlert } from "./alert";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+
+// Get posts
+export const getPosts = () => async (dispatch) => {
+  try {
+    const res = await axios.get("/api/post");
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add like
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/post/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Remove like
+export const removeLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/post/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete Post
+export const deletePost = (postId) => async (dispatch) => {
+  if (window.confirm("Are you sure you want to delete the post?")) {
+    try {
+      const res = await axios.delete(`/api/post/${postId}`);
+
+      dispatch({
+        type: DELETE_POST,
+        payload: postId,
+      });
+
+      dispatch(setAlert("Post Removed", "success"));
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  }
+};
